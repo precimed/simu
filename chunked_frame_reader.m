ffce5189cbb440047d14a253da46ee94dc5e4683
Oleksandr Frei <oleksandr.frei@gmail.com>
2017-02-17 18:24:48 +0100

    Strict validation of chunks file size

--- chunked_frame_reader.m
+++ chunked_frame_reader.m
@@ -16,23 +16,21 @@ function genomat = chunked_frame_reader(subjvec, snpvec, nsubj, snpdetailpath, g
     if isempty(PRS_genotypepath)  || ~strcmp(PRS_genotypepath,  genotypepath),  PRS_chunk_file = []; end;
     
     if isempty(PRS_chunk_file)
-        PRS_snpdetailpath = snpdetailpath; PRS_genotypepath = genotypepath;
         snp_detail = load(snpdetailpath);
         PRS_chunk_file = {};
         PRS_chunk_of_snp = nan(sum(snp_detail.snp_size(:)), 1);       
         PRS_chunk_index_of_snp = nan(sum(snp_detail.snp_size(:)), 1);  
         snp_index = 0;
         fprintf('Verifying integrity of chunks... ');
+        ok = true;
         for i=1:snp_detail.nchrom
         for j=1:snp_detail.nchunk(i)
             fileprefix=sprintf('%s/chr%d_chunk%d',genotypepath,i,j);
             bedprefix=[fileprefix,'.bed'];
-            if ~exist(bedprefix,'file') && (snp_detail.snp_size(i, j) > 0)
-                error('123');
-            end
             snps_per_chunk = snp_detail.snp_size(i, j);
-            if snp_detail.snp_size(i, j) > 0
-                if ~exist(bedprefix,'file'), error('Expected %s file not found --- %i SNPs lost', bedprefix, snps_per_chunk); end
+            if snps_per_chunk > 0
+                if ~exist(bedprefix,'file'), ok = false; fprintf('Expected %s file not found --- %i SNPs lost\n', bedprefix, snps_per_chunk); end
+                dirinfo = dir(bedprefix); if dirinfo.bytes ~= 3 + ceil(nsubj/4) * snps_per_chunk, ok = false; fprintf('File %s has size %i, expected %i\n', bedprefix, dirinfo.bytes, 3 + ceil(nsubj/4) * snps_per_chunk); end
                 PRS_chunk_file{end+1, 1} = fileprefix;
                 PRS_chunk_of_snp((snp_index + 1) : (snp_index + snps_per_chunk)) = length(PRS_chunk_file);
                 PRS_chunk_index_of_snp((snp_index + 1) : (snp_index + snps_per_chunk)) = 1:snps_per_chunk;
@@ -40,7 +38,9 @@ function genomat = chunked_frame_reader(subjvec, snpvec, nsubj, snpdetailpath, g
             end
         end
         end
+        if ~ok, error('Errors found. See messages above for more details.'); end;
         fprintf('OK. Found %i chunks with %i SNPs in total.', length(PRS_chunk_file), snp_index);
+        PRS_snpdetailpath = snpdetailpath; PRS_genotypepath = genotypepath;
     end
     
     if islogical(snpvec), snpvec = find(snpvec); end;
